e30479ec0d09a4ebbf484f35e416c1ce69263921
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2022-04-18 21:15:17 +0200

    Solve first kata

--- KataPractice1/src/FizzBuzz.ts
+++ KataPractice1/src/FizzBuzz.ts
@@ -1,1 +1,32 @@
-export class FizzBuzz {}
+export class FizzBuzz {
+  private printer: (line: string) => void;
+
+  constructor(printer: (line: string) => void) {
+    this.printer = printer;
+  }
+
+  run(number: number) {
+    const line: string = this.getLine(number);
+
+    this.printer(line);
+  }
+
+  private getLine(number: number) {
+    const isMultipleOfThree = number % 3 === 0;
+    const isMultipleOfFive = number % 5 === 0;
+
+    if (isMultipleOfThree && isMultipleOfFive) {
+      return 'FizzBuzz';
+    }
+
+    if (isMultipleOfThree) {
+      return 'Fizz';
+    }
+
+    if (isMultipleOfFive) {
+      return 'Buzz';
+    }
+
+    return number.toString();
+  }
+}
--- KataPractice1/test/FizzBuzz.spec.ts
+++ KataPractice1/test/FizzBuzz.spec.ts
@@ -1,3 +1,39 @@
+import { FizzBuzz } from '../src/FizzBuzz';
+
 describe('FizzBuzz', () => {
-  it('should ', () => {})
-})
+  let printer: any;
+  let fizzBuzz: FizzBuzz;
+
+  beforeEach(() => {
+    printer = jest.fn();
+    fizzBuzz = new FizzBuzz(printer);
+  });
+
+  afterEach(() => {
+    expect(printer).toHaveBeenCalledTimes(1);
+  });
+
+  it('should print the given number', () => {
+    fizzBuzz.run(2);
+
+    expect(printer).toHaveBeenCalledWith('2');
+  });
+
+  it.each([3, 6, 9, 12])('should print fizz with %p', (number) => {
+    fizzBuzz.run(number);
+
+    expect(printer).toHaveBeenCalledWith('Fizz');
+  });
+
+  it.each([5, 10, 25])('should print buzz with %p', (number) => {
+    fizzBuzz.run(number);
+
+    expect(printer).toHaveBeenCalledWith('Buzz');
+  });
+
+  it.each([15, 30])('should print buzz with %p', (number) => {
+    fizzBuzz.run(number);
+
+    expect(printer).toHaveBeenCalledWith('FizzBuzz');
+  });
+});
